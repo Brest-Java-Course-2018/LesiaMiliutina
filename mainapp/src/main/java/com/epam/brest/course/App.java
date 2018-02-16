6a39ca4629b045bfa2e5174359256a422cca84d6
Lesia Miliutina <lessmil2409@gmail.com>
2018-02-16 21:02:43 +0300

    Create table/Insert/Select operations

--- mainapp/src/main/java/com/epam/brest/course/App.java
+++ mainapp/src/main/java/com/epam/brest/course/App.java
@@ -1,13 +1,22 @@
 package com.epam.brest.course;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         System.out.println( "Hello World!" );
         DBUtils dbUtils = new DBUtils();
+        Connection connection = dbUtils.getConnection();
+        dbUtils.createUserTable(connection);
+        dbUtils.addUser(connection, "admin", "admin", "User admin");
+        dbUtils.addUser(connection, "admin1", "admin1", "User admin");
+        dbUtils.addUser(connection, "admin2", "admin2", "User admin");
+        dbUtils.getUsers(connection);
     }
 }
--- util/src/main/java/com/epam/brest/course/DBUtils.java
+++ util/src/main/java/com/epam/brest/course/DBUtils.java
@@ -1,19 +1,66 @@
 package com.epam.brest.course;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DBUtils {
 
     public Connection getConnection() throws ClassNotFoundException, SQLException {
         // URL
+        // 'test_db' is name of database
+        System.out.println("Connect to database.");
         String databaseURL = "jdbc:h2:mem:test_db;MODE=MYSQL;DB_CLOSE_DELAY=-1";
 
         Class.forName("org.h2.Driver");
         Connection connection =
                 DriverManager.getConnection(databaseURL, "sa", "");
         return connection;
+    }
+
+    public void createUserTable(Connection connection) throws SQLException {
+        // create statement - select
+        // prepared - update, insert (parameterize)
+        // call - stored procedures
+        System.out.println("Create app_user table.");
+
+        String createTable =
+                "CREATE TABLE app_user (" +
+                        "user_id INT NOT NULL AUTO_INCREMENT," +
+                        "login VARCHAR(255) NOT NULL," +
+                        "password VARCHAR(255) NOT NULL," +
+                        "description VARCHAR(255) NULL," +
+                        "PRIMARY KEY(user_id))";
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(createTable);
+        }
+    }
+
+    public void addUser(Connection connection, String login, String password, String description) throws SQLException {
+
+        System.out.println(String.format("Add user: %s", login));
+
+        String newUser = "INSERT INTO app_user (login, password, description) VALUES (?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(newUser);
+        preparedStatement.setString(1, login);
+        preparedStatement.setString(2, password);
+        preparedStatement.setString(3, description);
+        preparedStatement.executeUpdate();
+
+    }
+
+    public void getUsers(Connection connection) throws SQLException {
+
+        System.out.println("Get users: ");
+        String getRecords = "SELECT user_id, login, description " +
+                "FROM app_user ORDER BY user_id";
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(getRecords);
+        while (resultSet.next()) {
+            System.out.println(String.format("User: %s, %s, %s",
+                    resultSet.getString("user_id"),
+                    resultSet.getString("login"),
+                    resultSet.getString("description")));
+        }
+
     }
 
 }
